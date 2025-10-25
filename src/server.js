f3af44ce8d8614135df2f5efbb73a7a37d1d754d
Adriyan <adrianadrian.aa613@gmail.com>
2025-10-26 06:45:57 +0700

    fix host: memperbaiki bug pada host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const route = require('./route');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
