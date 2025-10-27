20a672cc7342d682267b7430dacf5d660f542dfc
Adriyan <adrianadrian.aa613@gmail.com>
2025-10-27 22:14:35 +0700

    feat(migration): menambahkan tabel notes ke dalam database

---
+++ migrations/1761573979984_create-table-notes.js
@@ -0,0 +1,42 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(60)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(60)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true,
+    },
+    created_at: {
+      type: 'VARCHAR(40)',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'VARCHAR(40)',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('notes');
+};
