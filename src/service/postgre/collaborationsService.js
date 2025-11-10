9a0a34dbec6357df36c8189cffe668dfb18f7a1d
Adriyan <adrianadrian.aa613@gmail.com>
2025-11-10 22:56:23 +0700

    feat(service): menambahkan CollaborationsService yang menangani fitur kolaborasi antar user

---
+++ src/service/postgre/collaborationsService.js
@@ -0,0 +1,53 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exception/InvariantError');
+const NotFoundError = require('../../exception/NotFoundError');
+
+class CollaborationsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addCollaboration(noteId, userId) {
+    const id = `collab-${nanoid(10)}`;
+    const query = {
+      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+      values: [id, noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async deleteCollaboration(noteId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE noteId = $1 AND userId = $2 RETURNING id',
+      values: [noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Kolaborasi gagal dihapus');
+    }
+  }
+
+  async verifyCollaborator(noteId, userId) {
+    const query = {
+      text: 'SELECT * FROM collaborations WHERE noteId = $1 AND userId = $2',
+      values: [noteId, userId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal diverifikasi');
+    }
+  }
+}
+
+module.exports = CollaborationsService;
